e3cc4c9d7079fad14d3acc2bd587c7c3e1e4eca9
Jallan Ferris <jallan.ferris@outlook.com>
2022-08-26 22:19:16 +1000

    Import required packages and CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const options = {
+    origin: '*',
+    headers: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: true,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  };
+  app.enableCors(options);
+  await app.listen(5000);
 }
 bootstrap();
